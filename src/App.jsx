3c85d2583e13005a56adaa8f3df08b65fa4ee130
kaziabulhasib <hasibkazi420@gmail.com>
2024-09-29 12:02:26 +0530

    add feat: text on page, clear text, copy text

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+// import "./App.css";
+import "./index.css";
 
 function App() {
   const [text, setText] = useState("");
 
-  // Load saved content on mount
   useEffect(() => {
     const savedText = localStorage.getItem("text");
     if (savedText) {
@@ -12,23 +12,44 @@ function App() {
     }
   }, []);
 
-  // Save content in local storage when text changes
   useEffect(() => {
     localStorage.setItem("text", text);
   }, [text]);
 
   const clearText = () => setText("");
 
+  const copyText = () => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        // alert("Text copied to clipboard!");
+      })
+      .catch((error) => {
+        console.error("Error copying text: ", error);
+      });
+  };
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className='container'>
-      <h1 className='text-red-500'>Hello world</h1>
-      <textarea
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder='Start writing...'
-      />
-      <button onClick={clearText}>Clear Text</button>
-      {/* Add more buttons for exporting if needed */}
+    <div className='container  mx-auto '>
+      <h1 className=' text-center text-3xl my-8'>
+        Welcome to webnote , write your text
+      </h1>
+      <div className='container mx-auto  px-8'>
+        <textarea
+          className='w-full h-[80vh] outline-none text-2xl bg-none '
+          value={text}
+          onChange={handleTextChange}
+          placeholder='Start writing...'
+        />
+      </div>
+      <div className='flex justify-center gap-6 '>
+        <button onClick={clearText}>Clear Text</button>
+        <button onClick={copyText}>Copy Text</button>
+      </div>
     </div>
   );
 }
